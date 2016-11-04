df19d848ecf7568957e3e365ee28688ae43d2781
André Puel <andrepuel@gmail.com>
2016-11-04 18:27:21 -0200

    Removed type unsafe casts

--- source/upromised/tcp.d
+++ source/upromised/tcp.d
@@ -46,6 +46,17 @@ void gcrelease(T)(T a) {
 T getSelf(T,Y)(Y* a) {
 	return cast(T)((cast(void*)a) - T.self.offsetof);
 }
+
+private uv_stream_t* stream(ref uv_tcp_t self) {
+    return cast(uv_stream_t*)&self;
+}
+private uv_handle_t* handle(ref uv_tcp_t self) {
+    return cast(uv_handle_t*)&self;
+}
+private sockaddr* upcast(ref sockaddr_in self) {
+    return cast(sockaddr*)&self;
+}
+
 class TcpSocket {
 private:
 	uv_loop_t* ctx;
@@ -65,7 +76,7 @@ public:
 		assert(closePromise is null);
 		closePromise = new Promise!void;
 		scope(failure) closePromise = null;
-		uv_close(cast(uv_handle_t*)&self, (selfSelf) {
+		uv_close(self.handle, (selfSelf) {
 			auto self = getSelf!TcpSocket(selfSelf);
 			self.closePromise.resolve();
 			gcrelease(self);
@@ -78,17 +89,17 @@ public:
 
 		sockaddr_in addr;
 		uv_ip4_addr(addrStr.toStringz, port, &addr).uvCheck();
-		uv_tcp_bind(&self, cast(sockaddr*)&addr, 0).uvCheck();
+		uv_tcp_bind(&self, addr.upcast, 0).uvCheck();
 	}
 
 	PromiseIterator!TcpSocket listen(int backlog) {
 		assert(listenPromise is null);
 		listenPromise = new PromiseIterator!TcpSocket;
-		uv_listen(cast(uv_stream_t*)&self, backlog, (selfSelf, status) {
+		uv_listen(self.stream, backlog, (selfSelf, status) {
 			enforce(status == 0);
 			auto self = getSelf!TcpSocket(selfSelf);
 			auto conn = new TcpSocket(self.ctx);
-			uv_accept(cast(uv_stream_t*)&self.self, cast(uv_stream_t*)&conn.self).uvCheck();
+			uv_accept(self.self.stream, conn.self.stream).uvCheck();
 			self.listenPromise.resolve(conn);
 		}).uvCheck();
 		return listenPromise;
@@ -107,10 +118,10 @@ public:
         }
         if (buf.base !is null) gcrelease(buf.base);
         enforce(nread >= 0);
-        uv_read_stop(cast(uv_stream_t*)&self.self);
+        uv_read_stop(self.self.stream);
         self.readPromise.resolve(cast(ubyte[])buf.base[0..nread]).then((cont) {
             if (cont) {
-                uv_read_start(cast(uv_stream_t*)&self.self, &readAlloc, &readCb).uvCheck();
+                uv_read_start(self.self.stream, &readAlloc, &readCb).uvCheck();
             } else {
                 scope(failure) fatal();
                 self.readPromise = null;
@@ -120,7 +131,7 @@ public:
 	PromiseIterator!(const(ubyte)[]) read() {
 		assert(readPromise is null);
 		readPromise = new PromiseIterator!(const(ubyte)[]);
-		uv_read_start(cast(uv_stream_t*)&self, &readAlloc, &readCb).uvCheck();
+		uv_read_start(self.stream, &readAlloc, &readCb).uvCheck();
 		return readPromise;
 	}
 
@@ -130,7 +141,7 @@ public:
 		scope(failure) gcrelease(r);
 		r.data.base = cast(char*)data.ptr;
 		r.data.len = data.length;
-		uv_write(&r.self, cast(uv_stream_t*)&self, &r.data, 1, (rSelf, status) {
+		uv_write(&r.self, self.stream, &r.data, 1, (rSelf, status) {
 			auto r = getSelf!WritePromise(rSelf);
 			gcrelease(r);
 			enforce(status == 0);
@@ -147,7 +158,7 @@ public:
 		ShutdownPromise r = new ShutdownPromise;
 		gcretain(r);
 		scope(failure) gcrelease(r);
-		uv_shutdown(&r.self, cast(uv_stream_t*)&self, (rSelf, status) {
+		uv_shutdown(&r.self, self.stream, (rSelf, status) {
 			auto r = getSelf!ShutdownPromise(rSelf);
 			gcrelease(r);
 			enforce(status == 0);
@@ -168,7 +179,7 @@ public:
 		ConnectPromise r = new ConnectPromise;
 		gcretain(r);
 		scope(failure) gcrelease(r);
-		uv_tcp_connect(&r.self, &self, cast(sockaddr*)&addr, (rSelf, status) {
+		uv_tcp_connect(&r.self, &self, addr.upcast, (rSelf, status) {
 			auto r = getSelf!ConnectPromise(rSelf);
 			gcrelease(r);
 			enforce(status == 0);
