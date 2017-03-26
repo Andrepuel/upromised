61c0dd7dbb508d40373de3d872ffb612b490b5bd
André Puel <andrepuel@gmail.com>
2017-03-25 22:39:54 -0300

    Removes shutdown of TTY
    There is no shutdown for TTY, so the shutdown() promise for TtyStream method returns right away.

--- source/upromised/tty.d
+++ source/upromised/tty.d
@@ -2,6 +2,7 @@ module upromised.tty;
 import deimos.libuv.uv;
 import deimos.libuv._d;
 import std.stdio : File;
+import upromised.promise : Promise;
 import upromised.uv : uvCheck;
 import upromised.uv_stream : UvStream;
 
@@ -12,5 +13,9 @@ public:
 
 		super(ctx);
 		uv_tty_init(ctx, &self, tty.fileno, tty == stdin).uvCheck();
+	}
+
+	override Promise!void shutdown() nothrow {
+		return Promise!void.resolved();
 	}
 }
