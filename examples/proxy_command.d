b0bd2ec69b3de342363deda5f1ac10d8e85d5c4d
André Puel <andrepuel@gmail.com>
2017-07-04 10:23:04 -0300

    Added proxy command example
    proxy command receive two args list (splited by --). The first is the proxy command, the command that will do the actual connection (using netcat protocol). The second command is the command that will be invoked, %h and %p will be replaced for the hostname and port of a TCP server that will proxy the connection using the specified proxy command.

---
+++ examples/proxy_command.d
@@ -0,0 +1,67 @@
+#!/usr/bin/env dub
+/+ dub.json:
+{
+	"name": "holepunch",
+	"dependencies": {
+		"upromised": {
+			"version": "*",
+			"path": "../"
+		}
+	}
+}
++/
+
+import std.exception : enforce;
+import std.stdio : stderr;
+import upromised.loop : defaultLoop;
+import upromised.stream : Stream;
+import upromised.pipe : Pipe;
+import upromised.process : Process;
+import upromised.promise : Promise, PromiseIterator;
+
+Promise!void forward(PromiseIterator!(const(ubyte)[]) from, Stream to) {
+	return from.each((data) => to.write(data.idup)).then((_) => to.shutdown());
+}
+
+int main(string[] args) {
+	import std.algorithm : countUntil, map;
+	import std.array : array;
+	import std.conv : to;
+	import std.random : uniform;
+	import std.string : replace, string;
+
+	auto sep = args.countUntil("--");
+	enforce(sep >= 0, "Expected -- paramater separating command from the proxy");
+	ushort port = cast(ushort)uniform(0x400, 0x10000);
+	string[] proxyArgs = args[1..sep];
+	string[] commandArgs = args[sep+1..$].map!(x => x.replace("%h", "127.0.0.1").replace("%p", port.to!string)).array;
+
+	auto loop = defaultLoop();
+	loop.resolve("127.0.0.1", port)
+	.then((addr) => loop.listenTcp(addr).each((conn) {
+		Pipe cin = new Pipe(loop);
+		Pipe cout = new Pipe(loop);
+		
+		Process proxy = new Process(loop, proxyArgs, cin, cout, Process.STDERR);
+		auto paral = forward(conn.read(), cin);
+
+		forward(cout.read(), conn)
+		.then(() => paral)
+		.finall(() => cin.close())
+		.finall(() => cout.close())
+		.finall(() => conn.close())
+		.except((Exception e) {
+			stderr.writeln(e);
+		}).nothrow_();
+	})).nothrow_();
+
+	Process command = new Process(loop, commandArgs, Process.STDIN, Process.STDOUT, Process.STDERR);
+	int r;
+	command.wait()
+	.then((rArg) {
+		r = cast(int)rArg;
+	}).nothrow_();
+
+	loop.run();
+	return r;
+}
