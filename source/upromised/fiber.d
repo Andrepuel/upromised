c10e2303fa481afe568e742424dea2f6f819f107
André Puel <andrepuel@gmail.com>
2017-09-04 01:44:53 -0300

    Async within async reuses the same fiber
    If an async function is await from within another async function, then the same Fiber is used to execute the inner function.

--- source/upromised/fiber.d
+++ source/upromised/fiber.d
@@ -2,16 +2,52 @@ module upromised.fiber;
 import upromised.promise : DelegatePromise, Promise;
 import core.thread : Fiber;
 
-Promise!T async(T)(T delegate() cb) {
-	import upromised.promise : promisifyCall;
+class FiberPromise(T) : DelegatePromise!T {
+private:
+	bool running;
+	T delegate() task;
 
-	auto r = new DelegatePromise!T;
-	(new Fiber(() nothrow {
-		promisifyCall(cb).thenWithTrace((v) => r.resolve(v));
-	})).call();
-	return r;
+public:
+	this(T delegate() task) {
+		this.task = task;
+	}
+
+	override protected void then_(void delegate(Value) nothrow cb) nothrow {
+		if (!running) {
+			try {
+				(new Fiber(() nothrow {
+					execute();
+				})).call();
+			} catch(Exception) {
+				import core.stdc.stdlib : abort;
+				abort();
+			}
+		}
+
+		super.then_(cb);
+	}
+
+	void execute() nothrow {
+		import upromised.promise : promisifyCall;
+		
+		if (running) {
+			return;
+		}
+
+		running = true;
+		promisifyCall(task).thenWithTrace((v) => resolve(v));
+	}
+}
+
+Promise!T async(T)(T delegate() cb) {
+	return new FiberPromise!T(cb);
 }
 T await(T)(Promise!T a) {
+	auto otherFiber = cast(FiberPromise!T)a;
+	if (otherFiber !is null) {
+		otherFiber.execute();
+	}
+
 	auto self = Fiber.getThis();
 	assert(self !is null);
 	
@@ -38,7 +74,7 @@ T await(T)(Promise!T a) {
 	if (r.e) {
 		throw r.e;
 	}
-	
+
 	static if (!is(T == void)) {
 		return r.value[0];
 	}
@@ -107,4 +143,78 @@ unittest {
 	}).nothrow_();
 
 	assert(value == 3);
+}
+//Call then twice on an async will yield the same result
+unittest {
+	auto res = async(() {
+		return new int;
+	});
+
+	int* v1;
+	int* v2;
+	res.then((v) {
+		v1 = v;
+	}).nothrow_();
+	res.then((v) {
+		v2 = v;
+	}).nothrow_();
+
+	assert(v1 !is null);
+	assert(v1 is v2);
+}
+//Async inside async
+unittest {
+	int v;
+	DelegatePromise!int dg = new DelegatePromise!int;
+	async(() {
+		return await(async(() {
+			return await(dg);
+		}));
+	}).then((arg) {
+		v = arg;
+	}).nothrow_();
+	assert(v == 0);
+	dg.resolve(3);
+	assert(v == 3);
+}
+//Await already fulfilled async
+unittest {
+	int v;
+	async(() {
+		auto other = async(() {
+			return 3;
+		});
+		other.then((arg) {
+			assert(v == 0);
+			v = arg;
+		}).nothrow_();
+		assert(v == 3);
+		v = 0;
+		int v2 = await(other);
+		assert(v == 0);
+		return v2;
+	}).then((arg) {
+		v = arg;
+	}).nothrow_;
+	assert(v == 3);
+}
+//Async within async reuses the same fiber
+unittest {
+	int called;
+	async(() {
+		auto outter = Fiber.getThis();
+		await(async(() {
+			assert(Fiber.getThis() is outter);
+			called++;
+		}));
+		assert(called == 1);
+
+		auto paralel = async(() {
+			assert(Fiber.getThis() !is outter);
+			called++;
+		}).nothrow_;
+		assert(called == 2);
+		called++;
+	}).nothrow_();
+	assert(called == 3);
 }
